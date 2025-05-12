80456e41780ee2dd9642e727675f5016f5250ed6
JoaoCDVancea <joao.vancea@sptech.school>
2025-05-11 23:01:52 -0300

    função cadastro concluída

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,26 @@
+var usuarioModel = require("../models/usuarioModel");
+
+function cadastrar(req, res) {
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+    
+    usuarioModel.cadastrar(nome, email, senha).then(
+        function(resultado) {
+            res.json(resultado);
+        }
+    ).catch (
+        function(erro) {
+            console.log(erro);
+            console.log(
+                "\nHouve um erro ao realizar o cadastro! Erro: ",
+                erro.sqlMessage
+            );
+            res.status(500).json(erro.sqlMessage);
+        }
+    );
+}
+
+module.exports = {
+    cadastrar
+}
