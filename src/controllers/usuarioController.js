14c11949a5e11b4dcefd4ac53552f0d0eb628de1
JoaoCDVancea <joao.vancea@sptech.school>
2025-05-12 15:51:26 -0300

    Função autenticação concluída

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -4,13 +4,13 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    
+
     usuarioModel.cadastrar(nome, email, senha).then(
-        function(resultado) {
+        function (resultado) {
             res.json(resultado);
         }
-    ).catch (
-        function(erro) {
+    ).catch(
+        function (erro) {
             console.log(erro);
             console.log(
                 "\nHouve um erro ao realizar o cadastro! Erro: ",
@@ -21,6 +21,41 @@ function cadastrar(req, res) {
     );
 }
 
+function autenticar(req, res) {
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    usuarioModel.autenticar(email, senha).then(
+        function (resultadoAutenticar) {
+            console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+            console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+            if (resultadoAutenticar.length == 1) {
+                console.log(resultadoAutenticar);
+
+                res.json({
+                    idUsuario: resultadoAutenticar[0].idUsuario,
+                    email: resultadoAutenticar[0].email,
+                    nome: resultadoAutenticar[0].nome,
+                    senha: resultadoAutenticar[0].senha,
+                });
+
+            } else if (resultadoAutenticar.length == 0) {
+                res.status(403).send("Email e/ou senha inválido(s)");
+            } else {
+                res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+            }
+        }
+    ).catch(
+        function (erro) {
+            console.log(erro);
+            console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+            res.status(500).json(erro.sqlMessage);
+        }
+    )
+}
+
 module.exports = {
-    cadastrar
+    cadastrar,
+    autenticar
 }
