6989312f87309f8259041f0141e412f559c85069
JoaoCDVancea <joao.vancea@sptech.school>
2025-06-11 20:34:20 -0300

    melhorias no css da navbar e implementação da logo

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,17 +2,51 @@ gerarNavbar();
 
 async function gerarNavbar() {
     const isAdmin = await verificarAdmin();
-    
+    let texto = '';
+
+    texto = `
+        <img src="../assets/imgs/vanchessLogo.png" id="logo">
+        <div id="paginas">
+            <div class="navbar-link" onclick="navegarPagina('desafios')" id="desafiosNav">
+                <p>Desafios</p>
+                <img src="../assets/icon/botaoDesafios.svg" alt="">
+            </div>
+            <div class="navbar-link" onclick="navegarPagina('artigos')" id="artigosNav">
+                <p>Artigos</p>
+                <img src="../assets/icon/botaoArtigosBranco.svg" alt="">
+            </div>
+    `;
+
     if(isAdmin == 1) {
-        navbar.innerHTML += `
-            <a href="../dashboard/dashboard.html"><div class="navbar-link">
+        texto += ` 
+            <div class="navbar-link" onclick="navegarPagina('dashboard')" id="dashboardNav">
                 <p>Dashboard</p>
-            </div></a>
-            <a href="../dashboard/configuracoes.html"><div class="navbar-link">
+                <img src="../assets/icon/dash.svg">
+            </div>
+            <div class="navbar-link" onclick="navegarPagina('configuracoes')" id="configuracoesNav">
                 <p>Configurações</p>
-            </div></a> 
+                <img src="../assets/icon/configuracoes.svg">
+            </div>
         `;
     }
+
+    texto += `
+        </div>
+        <div class="navbar-link" id="desconectar" onclick="desconectar()">
+            <p>Desconectar</p>
+            <img src="../assets/icon/logout.svg">
+        </div>
+    `;
+    
+    navbar.innerHTML = texto;
+
+    let paginaSelecionado = document.getElementById(`${sessionStorage.PAGINA}Nav`);
+    paginaSelecionado.classList.add('selecionado');
+}
+
+function navegarPagina(pagina) {
+    window.location = `${pagina}.html`;
+    sessionStorage.PAGINA = `${pagina}`;
 }
 
 async function verificarAdmin() {
@@ -32,4 +66,9 @@ async function verificarAdmin() {
 
     const jsonAdmin = await respostaAdmin.json();
     return jsonAdmin[0].isAdmin;
+}
+
+function desconectar() {
+    window.location = '../index.html';
+    
 }
